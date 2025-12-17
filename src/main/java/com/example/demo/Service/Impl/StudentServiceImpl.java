e6ae7a1b789f55707b72c97df795ce7dbddd93de
bala-amypo <bala@amypo.in>
2025-12-17 06:56:15 +0000

    User commit

--- src/main/java/com/example/demo/Service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/Service/Impl/StudentServiceImpl.java
@@ -8,5 +8,21 @@ public class StudentServiceImpl implements StudentService {
     private long counter=1;
 
     @Override
-    public
+    public Student insertStudent(Student st){
+        st.setId(counter++);
+        store.put(st.getId(),st);
+        return st;
+    }
+    @Override
+    public List<Student> getAllStudents(){
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public Optional<Student> getOneStudent(Long id){
+        return optional.ofNullable(store.get(id));
+    }
+    @Override
+    public void deleteStudent(Long id){
+        store.remove(id);
+    }
 }
