bcd1deb0880c75628e8291b020226b618cc509fe
bala-amypo <bala@amypo.in>
2025-12-17 06:26:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studentctl.java
+++ src/main/java/com/example/demo/controller/Studentctl.java
@@ -3,4 +3,22 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.Studententity;
-import com.example.demo.service.Studententity
+import com.example.demo.service.Studentservice;
+
+@RestController
+@ReqestMapping("/student")
+public class Studentctl{
+    @Autowired
+    private Studentservice ser;
+
+    //POST
+    @PostMapping Studententity addStudent(@ReqestBody Studententity st) {
+        return ser.saveData(st);
+    }
+    //GET ALL
+    @GetMapping("/getall")
+    public Collection<Studententity> getAllStudents() {
+        return ser.getAll();
+    }
+    //GET BY 
+}
